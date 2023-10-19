13299a8b32ec3e7dfb0988dec8d9ca6db410374a
IvanLasitsa <87463716+IvanLasitsa@users.noreply.github.com>
2023-10-19 20:42:52 +0300

    Add files via upload

--- python prac/task 7.py
+++ python prac/task 7.py
@@ -1,7 +1,6 @@
-i = 13
 list = [-5, 2, 10, -24, 15, -6, -52, 24, -37, 13, -2, -7, -11]
 
-for i in range(i):
+for i in range(13):
  if list[i] < 0:
     list[i] = abs(list[i])
     
--- python prac/task 8.py
+++ python prac/task 8.py
@@ -1,7 +1,6 @@
-array = 10
 list = []
 
-for i in range(array):
+for i in range(13):
  number = int(input(f"Введіть {i + 1}-й елемент масиву: "))
  list.append(number)
  
